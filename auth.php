763aadbfd804b36e65abf5264f6f6a6cb9a3b67e
aqiinatsir <aqiinatsir@gmail.com>
2026-03-06 09:54:33 +0700

    auth.php

--- auth.php
+++ auth.php
@@ -5,7 +5,7 @@
 session_start();
 
 define('DB_HOST',    'localhost');
-define('DB_NAME',    'wisata');
+define('DB_NAME',    'wisata_daerah');
 define('DB_USER',    'root');
 define('DB_PASS',    '');
 define('DB_CHARSET', 'utf8mb4');
